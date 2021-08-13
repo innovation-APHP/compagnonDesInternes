4227f1d82c0b76f7a9d70ec682865147a7992ab9
innovation-APHP <88909274+innovation-APHP@users.noreply.github.com>
2021-08-13 23:33:32 +0200

    Add files via upload

---
+++ assets/js/helpers/writeServices.vue
@@ -0,0 +1,46 @@
+// <template>
+//   <div>
+//     <div v-for="status of statuses" :key="status">{{status}}</div>
+//   </div>
+// </template>
+
+// <script>
+// import axios from 'axios'
+// import services from '~/static/convertcsv.json'
+
+// export default {
+//   data() {
+//     return {
+//       statuses: ['Running']
+//     }
+//   },
+//   mounted() {
+//     this.start()
+//   },
+//   methods: {
+//     async start() {
+//       for (const service of services) {
+//         const id = await this.getHospital(service.hospitalCode)
+//         console.log(id)
+//         axios
+//           .post(`${process.env.strapiURI}/services`, {
+//             nameSirius: service.nameSirius,
+//             nameBasins: service.nameBasins,
+//             code: service.code,
+//             hospital: id
+//           })
+//           .then(response => {
+//             this.statuses.push(`Done ${service.nameSirius} at ${id}`)
+//           })
+//           .catch(error => {
+//             this.statuses.push(`An error occurred: ${error}`)
+//           })
+//       }
+//     },
+//     async getHospital(code) {
+//       const hospital = await axios.get(`${process.env.strapiURI}/hospitals?code=${code}`)
+//       return hospital.data && hospital.data[0] ? hospital.data[0]._id : null
+//     }
+//   }
+// }
+// </script>
